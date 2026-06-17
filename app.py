f681d57282af4659bad9cf5efee80d15510ce3cd
Rathod Jaymitsinh <nrndrsnhrathod@gmail.com>
2026-06-17 11:23:46 +0530

    Enhance comments in home route for clarity on app.py code
    Added comments to clarify the functionality of the home route and input handling.

--- app.py
+++ app.py
@@ -1,21 +1,22 @@
+#Flask creates the web server 
 from flask import Flask, render_template, request
 from seo_audit import analyze_website
 
 app = Flask(__name__)
 
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/", methods=["GET", "POST"]) #That "@app.route" Line means " Open the function when user visit the home page " and " get post " method line means the page can be opened and also receive data
 def home():
 
     results = None
 
     if request.method == "POST":
 
-        url = request.form["url"]
+        url = request.form["url"] #reads the text typed into the input box.
 
         results = analyze_website(url)
 
-    return render_template(
+    return render_template( #sends data to the HTML page.
         "index.html",
         results=results
     )
