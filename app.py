cb599062e73cd49a7838f500d6c88c4c897e2e62
Jaymit-3672 <nrndrsnhrathod@gmail.com>
2026-06-17 17:47:29 +0530

    add pdf

--- app.py
+++ app.py
@@ -1,26 +1,79 @@
-#Flask creates the web server 
-from flask import Flask, render_template, request
+# ==========================================
+# IMPORT LIBRARIES
+# ==========================================
+
+from flask import (
+    Flask,
+    render_template,
+    request,
+    send_file
+)
+
 from seo_audit import analyze_website
+from report_generator import generate_pdf
+
+# ==========================================
+# CREATE FLASK APP
+# ==========================================
 
 app = Flask(__name__)
 
+# Store latest SEO results
+latest_results = None
 
-@app.route("/", methods=["GET", "POST"]) #That "@app.route" Line means " Open the function when user visit the home page " and " get post " method line means the page can be opened and also receive data
+# ==========================================
+# HOME PAGE
+# ==========================================
+
+@app.route("/", methods=["GET", "POST"])
 def home():
+
+    global latest_results
 
     results = None
 
     if request.method == "POST":
 
-        url = request.form["url"] #reads the text typed into the input box.
+        # Get URL from input box
+        url = request.form["url"]
 
+        # Run SEO audit
         results = analyze_website(url)
 
-    return render_template( #sends data to the HTML page.
+        # Save results for PDF export
+        latest_results = results
+
+    return render_template(
         "index.html",
         results=results
     )
 
+# ==========================================
+# DOWNLOAD PDF REPORT
+# ==========================================
+
+@app.route("/download-report")
+def download_report():
+
+    global latest_results
+
+    if latest_results is None:
+
+        return "Please analyze a website first."
+
+    pdf_file = generate_pdf(
+        latest_results
+    )
+
+    return send_file(
+        pdf_file,
+        as_attachment=True
+    )
+
+# ==========================================
+# RUN FLASK SERVER
+# ==========================================
 
 if __name__ == "__main__":
+
     app.run(debug=True)
